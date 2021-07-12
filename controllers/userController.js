6efe8e39d428c511363c81d9f600046bc3c4de6e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-12 17:25:43 -0300

    route for handling register and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,17 @@
 const Users=require('../models/Users')
-const db=require('../database/users.json')
 const userController={
     login:(req,res)=>{
         res.render('login')
     },
     register:(req,res)=>{
-        res.send(db)
-        /* Users.create(req.body)
         res.render('register')
-        return res.send('todo ok' + req.body) */
+    },
+    processRegister:(req,res)=>{
+        Users.create(req.body)
+        res.send('ok')
+    },
+    processLogin:(req,res)=>{
+
     },
 }
 module.exports=userController

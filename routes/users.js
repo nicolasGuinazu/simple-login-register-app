2a858483067473c03c526b78d9e3702847386080
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-12 17:26:25 -0300

    route for handling register and login

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ var router = express.Router();
 let userController=require('../controllers/userController')
 
 router.get('/login',userController.login);
+router.post('/login',userController.processLogin);
 router.get('/register',userController.register);
+router.post('/register',userController.processRegister);
 module.exports = router;

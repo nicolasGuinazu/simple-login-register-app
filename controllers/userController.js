7f723a598eaffd72f8fd5240f467be197cc4b7e6
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-13 11:25:17 -0300

    bcrypt to hash password, adding avatar to created user and testing for existing users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const Users=require('../models/Users')
+const bcrypt=require('bcrypt')
+
 const userController={
     login:(req,res)=>{
         res.render('login')
@@ -7,11 +9,28 @@ const userController={
         res.render('register')
     },
     processRegister:(req,res)=>{
-        Users.create(req.body)
-        res.send('ok')
+        let userInDb=Users.findByField('email',req.body.email)
+
+        if (userInDb){
+            return res.send('Usuario ya registrado!!') //es con validaciones pero para testear
+        }
+
+        let newUser={
+            ...req.body,
+            password:bcrypt.hashSync(req.body.password,10),
+            avatar:req.file.filename
+        }
+
+        Users.create(newUser)
+        res.redirect('/')
     },
     processLogin:(req,res)=>{
-
+        let userToLogIn=Users.findByField('email',req.body.email)
+        let passOk=bcrypt.compareSync(req.body.password,userToLogIn.password)
+        if (passOk){
+            return res.send('todo ok!')//es con validaciones pero para testear
+        }
+        res.send('not-ok')
     },
 }
 module.exports=userController

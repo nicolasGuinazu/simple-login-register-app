c67c9389e237a8519b46549f618f3990518a6b52
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-13 16:35:38 -0300

    middlewares added in register and login routes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 let userController=require('../controllers/userController')
 let upload=require('../middlewares/multerMiddleware')
+let guestMiddleware=require('../middlewares/guestMiddleware')
+let authMiddleware=require('../middlewares/authMiddleware');
 
-
-router.get('/login',userController.login);
+router.get('/login',guestMiddleware,userController.login);
 router.post('/login',userController.processLogin);
-router.get('/register',userController.register);
+router.get('/register',guestMiddleware,userController.register);
 router.post('/register',upload.single('avatar'),userController.processRegister);
-router.get('/profile',userController.profile);
+router.get('/profile',authMiddleware,userController.profile);
 module.exports = router;

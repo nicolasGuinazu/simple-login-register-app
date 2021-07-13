e797754196e14991bdfa77ca34123fbe247dfef7
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-13 11:27:10 -0300

    added multer middleware to register, to upload files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 let userController=require('../controllers/userController')
+let upload=require('../middlewares/multerMiddleware')
+
 
 router.get('/login',userController.login);
 router.post('/login',userController.processLogin);
 router.get('/register',userController.register);
-router.post('/register',userController.processRegister);
+router.post('/register',upload.single('avatar'),userController.processRegister);
 module.exports = router;

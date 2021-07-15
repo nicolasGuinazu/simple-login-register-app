99fbddf56809eda9202bab367ea499975d9df552
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-15 18:13:17 -0300

    added validation middleware in post route of the register

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ let userController=require('../controllers/userController')
 let upload=require('../middlewares/multerMiddleware')
 let guestMiddleware=require('../middlewares/guestMiddleware')
 let authMiddleware=require('../middlewares/authMiddleware');
+let validationsRegister=require('../middlewares/validationsRegister');
 
 router.get('/login',guestMiddleware,userController.login);
 router.post('/login',userController.processLogin);
 router.get('/register',guestMiddleware,userController.register);
-router.post('/register',upload.single('avatar'),userController.processRegister);
+router.post('/register',upload.single('avatar'),validationsRegister,userController.processRegister);
 router.get('/profile',authMiddleware,userController.profile);
 module.exports = router;

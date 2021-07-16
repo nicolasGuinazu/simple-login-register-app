cd1a213f4c3df89593d3e2406bf3d7ad01e48b2f
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-16 09:12:03 -0300

    sending errors and old body to view, adding a custom error when pasword its wrong

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,55 +1,70 @@
-const Users=require('../models/Users')
-const bcrypt=require('bcrypt')
-const {validationResult}=require('express-validator')
+const Users = require('../models/Users')
+const bcrypt = require('bcrypt')
+const {
+    validationResult
+} = require('express-validator')
 
-const userController={
-    login:(req,res)=>{
+const userController = {
+    login: (req, res) => {
         res.render('login')
     },
-    register:(req,res)=>{
+    register: (req, res) => {
         res.render('register')
     },
-    processRegister:(req,res)=>{
-       let userInDb=Users.findByField('email',req.body.email)
+    processRegister: (req, res) => {
+        let userInDb = Users.findByField('email', req.body.email)
 
-        if (userInDb){
+        if (userInDb) {
             return res.send('Usuario ya registrado!!') //es con validaciones pero para testear
-        } 
-        let errors=validationResult(req);
-        if (errors.isEmpty()){
-            let newUser={
+        }
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            let newUser = {
                 ...req.body,
-                password:bcrypt.hashSync(req.body.password,10),
-                avatar:req.file.filename
+                password: bcrypt.hashSync(req.body.password, 10),
+                avatar: req.file.filename
             }
-    
+
             Users.create(newUser)
             res.redirect('/')
-        }else{
-             res.render('register', {errors:errors.mapped(), old:req.body})
+        } else {
+            res.render('register', {
+                errors: errors.mapped(),
+                old: req.body
+            })
         }
-       
+
     },
-    processLogin:(req,res)=>{
-        let userToLogIn=Users.findByField('email',req.body.email)
-        let passOk=bcrypt.compareSync(req.body.password,userToLogIn.password)
-        if (passOk){
-            req.session.loggedUser=userToLogIn;
-            if(req.body.remember){
-                res.cookie('userEmail',req.body.email,{maxAge:(1000*6)*2})
+    processLogin: (req, res) => {
+        let userToLogIn = Users.findByField('email', req.body.email)
+        let passOk = bcrypt.compareSync(req.body.password, userToLogIn.password)
+        if (passOk) {
+            req.session.loggedUser = userToLogIn;
+            if (req.body.remember) {
+                res.cookie('userEmail', req.body.email, {
+                    maxAge: (1000 * 6) * 2
+                })
             }
         }
-        return res.redirect('/')
+        return res.render('login',{
+            errors: {
+                mail: {
+                    msg: 'Las credenciales son invalidas'
+                }
+            }
+        })
     },
-    profile:(req,res)=>{
+    profile: (req, res) => {
 
-    return res.render('profile',{user:req.session.loggedUser}) 
-      
+        return res.render('profile', {
+            user: req.session.loggedUser
+        })
+
     },
-    logout:(req,res)=>{
+    logout: (req, res) => {
         res.clearCookie('userEmail');
         req.session.destroy();
         res.redirect('/')
     }
 }
-module.exports=userController
+module.exports = userController

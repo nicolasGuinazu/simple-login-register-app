c3e377c43a1dc5e53c036377c1ed91e3d8b537f1
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-07-16 09:10:50 -0300

    fixed the error message from the email input

--- middlewares/validationsRegister.js
+++ middlewares/validationsRegister.js
@@ -2,7 +2,7 @@ const {check}=require('express-validator')
 
 module.exports=[
     check('name').notEmpty().withMessage('Debes escribir un nombre valido'),
-    check('email').notEmpty().withMessage('Debes escribir un nombre valido'),
+    check('email').notEmpty().withMessage('Debes escribir un email valido'),
     check('password').notEmpty().withMessage('Debes crear una contrasena')
 
 ]
